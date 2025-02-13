777abdf5a195b1a12f357ea78490939e889f7039
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-02-13 10:46:09 +0100

    do not cache pdfs

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         VitePWA({
             registerType: "autoUpdate",
             workbox: {
-                globPatterns: ["**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}"],
+                globPatterns: ["**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2,pdf}"],
                 // Ensure network-first strategy for API calls
                 runtimeCaching: [
                     {
